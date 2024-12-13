4cd735b46e49e4a80cd80aa8a94333c2be8d8b71
Адис <piv4484@gmail.com>
2024-12-13 16:22:01 +0600

    I changed it to the interface

--- src/PayMethodd/MoneyRes.java
+++ src/PayMethodd/MoneyRes.java
@@ -1,4 +1,24 @@
 package PayMethodd;
 
 public class MoneyRes implements PaymentMethod {
+    private int amount;
+
+    public MoneyRes() {
+        this.amount = 0;
+    }
+
+    @Override
+    public void addMoney(int money) {
+        this.amount += money;
+    }
+
+    @Override
+    public int getAmount() {
+        return this.amount;
+    }
+
+    @Override
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
 }
--- src/PayMethodd/PayCard.java
+++ src/PayMethodd/PayCard.java
@@ -1,4 +1,31 @@
 package PayMethodd;
 
 public class PayCard implements PaymentMethod {
+    private int amount;
+
+    public PayCard(int amount) {
+        this.amount = amount;
+    }
+    @Override
+    public void addMoney(int money) {
+        this.amount += money;
+    }
+
+    @Override
+    public int getAmount() {
+        return this.amount;
+    }
+
+    @Override
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public boolean payWithCard(int amountToPay) {
+        if (amountToPay > 0) {
+            this.amount += amountToPay;
+            return true;
+        }
+        return false;
+    }
 }
